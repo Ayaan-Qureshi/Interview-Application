d58c0e1515ffa94ab670bc3b849e0b99bd6b5abd
Ayaan-Qureshi <irfan.ayaan@outlook.com>
2025-11-19 02:39:39 +0500

    initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import path from "path"
+import { ENV } from './lib/env.js'
+
+const app = express()
+
+const __dirname= path.resolve()
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({msg:"successfully Connected to Health"})
+})
+
+app.get("/books", (req, res) => {
+    res.status(200).json({msg:"successfully Connected to Books"})
+})
+
+//make ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}", (req, res) =>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(ENV.PORT, ()=> console.log(`Server is running on http://localhost:${ENV.PORT}/`))
